79d0d9fdedc05f1924e0d08ffb3fa2067e5d2ed2
Henriete Čupāne <henriete.cupane@edu.rtu.lv>
2025-04-28 16:06:52 +0000

    2. daļa (28.04.2025)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 def ietaupi_naudu():
     print("Vēlies ietaupīt naudu? SĀKAM!")
     ienakumi = float(input("Ievadi savus mēneša ienākumus(€):"))
+    atlikusi_nauda = ienakumi #Lai sekotu, cik naudas paliek
     kategorijas = [
         "pārtika",
         "ēšana ārpus mājas (restorāni/fast food/kafejnīcas)","mājas izdevumi (komunālie + īre / nekustamā īpašuma nodoklis)",
@@ -17,14 +18,28 @@ def ietaupi_naudu():
         "dāvanas"
     ]
     limiti = []
-
     print("\nIevadi savus mēneša limitus (€):")
+
+    neparsniegts = False
     for kategorija in kategorijas:
+        if atlikusi_nauda == 0:
+             if not neparsniegts:
+                print(f"Esi pārsniedzis budetu. Atlikušajam limitam tiek piešķirts 0€")
+                neparsniegts = True
+             limiti.append(0)
+             continue
+        
         while True:
             try:
-                limits = float(input(f"{kategorija}: "))
-                limiti.append(limits)
-                break 
+                limits = float(input(f"{kategorija} (Tev atlikuši {atlikusi_nauda:.2f}€): "))
+                if limits < 0:
+                     print("Limits nevar būt negatīvs.  Ievadi no jauna.")
+                elif round(limits,2) > round(atlikusi_nauda, 2):
+                     print(f"Limits {limits:.2f}€ pārsniedz atlikušos ienākumus! Ievadi mazāku summu!")
+                else:
+                     limiti.append(limits)
+                     atlikusi_nauda = round(atlikusi_nauda - limits, 2)
+                     break 
             except ValueError:
                 print("Lūdzu ievadi skaitli!")
 
@@ -33,10 +48,15 @@ def ietaupi_naudu():
         'Mēneša limits (€)': limiti
     })
 
-    faila_nosaukums = "naudas_kopsavilkums.xlsx"
+    faila_nosaukums = "budzeta_limitu_kopsavilkums.xlsx"
     dati.to_excel(faila_nosaukums, index=False)
 
     print (f"\n Tavi limiti ir saglabāti! Tos vari atrast : {faila_nosaukums}")
+
+    if atlikusi_nauda > 0:
+         print(f"\nPēc budeta sastādīšanas Tev vēl paliek brīvi {atlikusi_nauda:.2f}€")
+    else:
+         print("\nVisi ienākumi ir sadalīti pa kategorijām!")
 
 if __name__ == "__main__":
        ietaupi_naudu() 
